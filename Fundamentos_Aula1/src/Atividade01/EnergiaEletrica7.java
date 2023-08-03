302461cea9eacc2d73b3b1f15ec1cf17c47f93ef
Rafaela <rafaela.barros2@aluno.senai.br>
2023-08-03 14:06:01 -0300

    Enviando a atividade

--- Fundamentos_Aula1/src/Atividade01/EnergiaEletrica7.java
+++ Fundamentos_Aula1/src/Atividade01/EnergiaEletrica7.java
@@ -8,6 +8,28 @@ public class EnergiaEletrica7 {
         System.out.println("Informe a quantidade de KW/h consumido: ");
         double KWh = scanner.nextDouble();
         System.out.println("Qual seu tipo de instalação elétrica: (R para residência, I para indústria e C para comércio)");
-        char Inst = scanner.next().charAt(0);
+        char inst = scanner.next().charAt(0);
+        if (inst == 'r'|| inst == 'R') {
+            if(KWh <= 500){
+            KWh = KWh * 0.40;
+            }
+            else{
+            KWh = KWh * 0.65;
+        }}
+            if (inst == 'c'|| inst == 'C') {
+            if(KWh <= 1000){
+            KWh = KWh * 0.55;
+            }
+            else{
+            KWh = KWh * 0.60;
+        }}
+            if (inst == 'i'|| inst == 'I') {
+            if(KWh <= 5000){
+            KWh = KWh * 0.55;
+            }
+            else{
+            KWh = KWh * 0.60;
+        }}
+                System.out.println("Valor a ser pago: " + KWh);
     }
 }
--- Fundamentos_Aula1/src/Atividade01/Media9.java
+++ Fundamentos_Aula1/src/Atividade01/Media9.java
@@ -1,5 +1,25 @@
 package Atividade01;
 
-public class Media9 {
+import java.util.Scanner;
 
+public class Media9 {
+    public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+    int rs = media(10,10,10,10);
+        System.out.println("A media:" + rs);
+        
+    }
+    
+    public static int media(int... valores) {
+    int resultado = 0;
+   
+        for (int i = 0; i < valores.length; i++) {
+            int valor = valores[i];
+            resultado += valor;
+        }
+        resultado = resultado / valores.length;
+        return resultado;
+    }
 }
+    
+
